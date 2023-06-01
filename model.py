7d008eba451963932df10f45224e4a204887f6d1
nhn1030 <shgusska12@gmail.com>
2023-06-02 01:16:23 +0900

    revise

--- model.py
+++ model.py
@@ -1,3 +1,11 @@
 import torch
 import torch.nn as nn
-import torch.nn.function as F
+import torch.nn.functional as F
+
+
+class VGGNet(nn.Module):
+    def __init__(self, num_classes = 1000):
+        super(VGGNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.conv2d(in_planes = 3, out_planes =64,)
+        )
