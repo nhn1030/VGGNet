b5cfc4a805b90a959f06366af22f4c38c68f33b9
nhn1030 <shgusska12@gmail.com>
2023-06-02 10:09:22 +0900

    revise

--- model.py
+++ model.py
@@ -15,11 +15,24 @@ class VGGBlock(nn.Module):
         layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         
         self.block = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.block(x)
+        return x
             
 
 class VGGNet(nn.Module):
-    def __init__(self, num_classes = 1000):
+    def __init__(self, num_classes=1000):
         super(VGGNet, self).__init__()
         self.features = nn.Sequential(
-            nn.conv2d(in_planes = 3, out_planes =64,)
+            VGGBlock(3, 64, 2),
+            VGGBlock(3, 128, 2),
+            VGGBlock(3, 256, 3),
+            VGGBlock(3, 512, 3),
+            VGGBlock(3, 512, 3),
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(512)
+        )
         
