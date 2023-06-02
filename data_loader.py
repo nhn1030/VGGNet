a761c42e9ea3f59436836f84d98677cf1a7fb043
nhn1030 <shgusska12@gmail.com>
2023-06-02 11:10:54 +0900

    revise

--- data_loader.py
+++ data_loader.py
@@ -2,15 +2,15 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-def get_train_loader(batch_size, num_workers):
+def get_train_loader(root, batch_size, num_workers):
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
     ])
-
+    
     train_dataset = torchvision.datasets.CIFAR10(
-        root='./datasets', train=True, download=True, transform=transform_train
+        root= root, train=True, download=True, transform=transform_train
     )
 
     train_loader = torch.utils.data.DataLoader(
@@ -19,13 +19,13 @@ def get_train_loader(batch_size, num_workers):
 
     return train_loader
 
-def get_test_loader(batch_size, num_workers):
+def get_test_loader(root, batch_size, num_workers):
     transform_test = transforms.Compose([
         transforms.ToTensor(),
     ])
-
+    
     test_dataset = torchvision.datasets.CIFAR10(
-        root='./datasets', train=False, download=True, transform=transform_test
+        root= root, train=False, download=True, transform=transform_test
     )
 
     test_loader = torch.utils.data.DataLoader(
@@ -33,3 +33,24 @@ def get_test_loader(batch_size, num_workers):
     )
 
     return test_loader
+
+
+root = 'datasets'  # 실제 데이터셋이 위치한 디렉토리 경로로 수정해주세요.
+batch_size = 64
+num_workers = 2
+
+train_loader = get_train_loader(root, batch_size, num_workers)
+test_loader = get_test_loader(root, batch_size, num_workers)
+# 데이터셋 사용 예시
+# for images, labels in train_loader:
+#     # 학습 데이터셋 사용
+#     # images: [batch_size, 3, 32, 32] 크기의 이미지 배치
+#     # labels: [batch_size] 크기의 정답 레이블 배치
+#     pass
+
+# for images, labels in test_loader:
+#     # 테스트 데이터셋 사용
+#     pass
+
+# train_loader = train_loader(batch_size=128, num_workers=2)
+# test_loader = test_loader(batch_size=128, num_workers=2)
