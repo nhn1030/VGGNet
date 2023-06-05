805a22991c746a72a7a6b92adda5356391e846e7
nhn1030 <shgusska12@gmail.com>
2023-06-05 14:36:32 +0900

    revise(cuda --> device)

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ from data_loader import get_train_loader
 from data_loader import get_test_loader
 
 if __name__ == '__main__':
-    root = 'datasets'  # 실제 데이터셋이 위치한 디렉토리 경로로 수정해주세요.
+    root = 'datasets'  
     batch_size = 64
     num_workers = 2
-    
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     train_loader = get_train_loader(root, batch_size, num_workers)
     test_loader = get_test_loader(root, batch_size, num_workers)
-    
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    net = VGGNet16()
+
+    net = VGGNet16().to(device)
 
     learning_rate = 0.001
     file_name = 'VGGNet16_CIFAR10.pth'
@@ -25,37 +26,37 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=learning_rate, weight_decay=0.0001)
 
-            
+
     def train(epoch):
-            print('\n[ Train epoch: %d ]' % epoch)
-            net.train()
-            train_loss = 0
-            correct = 0
-            total = 0
+        print('\n[ Train epoch: %d ]' % epoch)
+        net.train()
+        train_loss = 0
+        correct = 0
+        total = 0
 
-            for batch_idx, (inputs, targets) in enumerate(train_loader):
-                inputs, targets = inputs.to(device), targets.to(device)
-                optimizer.zero_grad()
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
 
-                outputs = net(inputs)
-                loss = criterion(outputs, targets)
-                loss.backward()
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
 
-                optimizer.step()
-                train_loss += loss.item()
-                _, predicted = outputs.max(1)
+            optimizer.step()
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
 
-                total += targets.size(0)
-                current_correct = predicted.eq(targets).sum().item()
-                correct += current_correct
-                
-                if batch_idx % 100 == 0:
-                    print('\nCurrent batch:', str(batch_idx))
-                    print('Current batch average train accuracy:', current_correct / targets.size(0))
-                    print('Current batch average train loss:', loss.item() / targets.size(0))
+            total += targets.size(0)
+            current_correct = predicted.eq(targets).sum().item()
+            correct += current_correct
 
-            print('\nTotal average train accuarcy:', correct / total)
-            print('Total average train loss:', train_loss / total)
+            if batch_idx % 100 == 0:
+                print('\nCurrent batch:', str(batch_idx))
+                print('Current batch average train accuracy:', current_correct / targets.size(0))
+                print('Current batch average train loss:', loss.item() / targets.size(0))
+
+        print('\nTotal average train accuarcy:', correct / total)
+        print('Total average train loss:', train_loss / total)
 
 
     def test(epoch):
@@ -65,15 +66,16 @@ if __name__ == '__main__':
         correct = 0
         total = 0
 
-        for batch_idx, (inputs, targets) in enumerate(test_loader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            total += targets.size(0)
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(test_loader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                total += targets.size(0)
 
-            outputs = net(inputs)
-            loss += criterion(outputs, targets).item()
+                outputs = net(inputs)
+                loss += criterion(outputs, targets).item()
 
-            _, predicted = outputs.max(1)
-            correct += predicted.eq(targets).sum().item()
+                _, predicted = outputs.max(1)
+                correct += predicted.eq(targets).sum().item()
 
         print('\nTotal average test accuarcy:', correct / total)
         print('Total average test loss:', loss / total)
